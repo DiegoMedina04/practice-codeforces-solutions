d12fada86ab4758e6f30b02d41848515fe73045d
Diego Prada <106110823+DiegoMedina04@users.noreply.github.com>
2023-03-25 00:25:44 -0500

    158A - Next Round

---
+++ 158A - Next Round.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int cont=0;
+    int n=sc.nextInt();
+    int k=sc.nextInt();
+    int[] score= new int[n];
+    for(int i=0; i<n; i++){
+      score[i]=sc.nextInt();
+    }
+    int position =score[k-1];
+    for(int i=0; i<score.length; i++){
+      cont+=(score[i]>=position && score[i]!=0)?1:0;
+      if(i==k-1)continue;
+    }
+    System.out.println(cont);
+  }
+}
